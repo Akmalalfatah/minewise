8d3308d3c939fe1a44fc98479bbe06fe79762e36
Naufal Fakhri <naufalfakhri1823@gmail.com>
2025-12-05 00:33:00 +0700

    refactor PortWeatherConditions to use semantic HTML structure

--- src/components/shipping-planner/PortWeatherConditions.jsx
+++ src/components/shipping-planner/PortWeatherConditions.jsx
@@ -27,7 +27,7 @@ function PortWeatherConditions() {
   const riskSubtitle = data?.riskSubtitle || "-";
 
   return (
-    <div
+    <section
       data-layer="port_weather_condition_card"
       className="PortWeatherConditionCard h-[492px] p-6 bg-white rounded-3xl inline-flex justify-center items-center gap-2.5"
     >
@@ -40,7 +40,8 @@ function PortWeatherConditions() {
           className="ContentContainer self-stretch flex flex-col justify-start items-start gap-3"
         >
 
-          <div
+          {/* Header */}
+          <header
             data-layer="header_container"
             className="HeaderContainer self-stretch flex flex-col justify-start items-start gap-2.5"
           >
@@ -48,7 +49,7 @@ function PortWeatherConditions() {
               data-layer="header_left_group"
               className="HeaderLeftGroup relative inline-flex justify-start items-center gap-3"
             >
-              <div
+              <figure
                 data-layer="icon_wrapper"
                 className="IconWrapper size-8 p-[7px] bg-[#1c2534] rounded-2xl flex justify-center items-center gap-2.5"
               >
@@ -57,10 +58,11 @@ function PortWeatherConditions() {
                   src="/icons/icon_weather.png"
                   alt="Weather icon"
                 />
-              </div>
-              <div className="PortWeatherConditionTitle text-black text-sm font-semibold">
+              </figure>
+
+              <h2 className="PortWeatherConditionTitle text-black text-sm font-semibold">
                 Port Weather Conditions
-              </div>
+              </h2>
 
               <button
                 type="button"
@@ -69,79 +71,85 @@ function PortWeatherConditions() {
                 <div className="IconFilter size-[21px] relative" />
               </button>
             </div>
-          </div>
+          </header>
 
-          <div className="AreaSectionContainer self-stretch inline-flex justify-between items-center">
-            <div className="AreaLabel text-black text-xs font-semibold">Area:</div>
-            <div className="AreaValue text-right text-black text-xs font-semibold">
+          {/* Area */}
+          <section className="AreaSectionContainer self-stretch inline-flex justify-between items-center">
+            <span className="AreaLabel text-black text-xs font-semibold">
+              Area:
+            </span>
+            <span className="AreaValue text-right text-black text-xs font-semibold">
               {area}
-            </div>
-          </div>
+            </span>
+          </section>
 
-          <div className="DividerTop self-stretch h-0 outline outline-[0.50px] outline-offset-[-0.25px] outline-[#bdbdbd]" />
+          <hr className="DividerTop self-stretch h-0 outline outline-[0.50px] outline-offset-[-0.25px] outline-[#bdbdbd]" />
 
-          <div className="WeatherInfoSection self-stretch h-64 inline-flex justify-between items-center">
-
-            <div className="LabelColumnContainer w-[87px] h-64 inline-flex flex-col justify-center items-center gap-5">
-              <div className="LabelListContainer self-stretch h-[253px] flex flex-col justify-start items-start gap-3 text-black text-sm font-semibold">
-                <div>Location</div>
-                <div>Rainfall</div>
-                <div>Temperature</div>
-                <div>Humidity</div>
-                <div>Wind</div>
-                <div>Pressure</div>
-                <div>Visibility</div>
-                <div>Lightning</div>
-                <div>Updated</div>
+          {/* Weather info as description list */}
+          <section className="WeatherInfoSection self-stretch h-64 inline-flex justify-between items-center">
+            <dl className="w-full flex justify-between">
+              {/* Labels */}
+              <div className="LabelColumnContainer w-[87px] h-64 inline-flex flex-col justify-center items-center gap-5">
+                <div className="LabelListContainer self-stretch h-[253px] flex flex-col justify-start items-start gap-3 text-black text-sm font-semibold">
+                  <dt>Location</dt>
+                  <dt>Rainfall</dt>
+                  <dt>Temperature</dt>
+                  <dt>Humidity</dt>
+                  <dt>Wind</dt>
+                  <dt>Pressure</dt>
+                  <dt>Visibility</dt>
+                  <dt>Lightning</dt>
+                  <dt>Updated</dt>
+                </div>
               </div>
-            </div>
 
-            <div className="ValueColumnContainer w-[117px] h-64 inline-flex flex-col justify-start items-end gap-5">
-              <div className="ValueListContainer self-stretch flex flex-col justify-start items-end gap-3 text-black text-sm font-semibold">
-                <div>{location}</div>
-                <div>{rainfall}</div>
-                <div>{temperature}</div>
-                <div>{humidity}</div>
-                <div>{wind}</div>
-                <div>{pressure}</div>
-                <div>{visibility}</div>
-                <div>{lightning}</div>
-                <div>{updated}</div>
+              {/* Values */}
+              <div className="ValueColumnContainer w-[117px] h-64 inline-flex flex-col justify-start items-end gap-5">
+                <div className="ValueListContainer self-stretch flex flex-col justify-start items-end gap-3 text-black text-sm font-semibold">
+                  <dd>{location}</dd>
+                  <dd>{rainfall}</dd>
+                  <dd>{temperature}</dd>
+                  <dd>{humidity}</dd>
+                  <dd>{wind}</dd>
+                  <dd>{pressure}</dd>
+                  <dd>{visibility}</dd>
+                  <dd>{lightning}</dd>
+                  <dd>{updated}</dd>
+                </div>
               </div>
-            </div>
-
-          </div>
+            </dl>
+          </section>
 
         </div>
 
-        <div className="DividerMiddle self-stretch h-0 outline outline-[0.50px] outline-offset-[-0.25px] outline-[#bdbdbd]" />
+        <hr className="DividerMiddle self-stretch h-0 outline outline-[0.50px] outline-offset-[-0.25px] outline-[#bdbdbd]" />
 
-        <div className="RiskSectionContainer self-stretch flex flex-col justify-start items-start gap-2.5">
-          <div className="RiskSectionTitle self-stretch text-black text-sm font-semibold">
+        {/* Risk section */}
+        <section className="RiskSectionContainer self-stretch flex flex-col justify-start items-start gap-2.5">
+          <h3 className="RiskSectionTitle self-stretch text-black text-sm font-semibold">
             Weather-Based Risk
-          </div>
+          </h3>
 
           <div className="RiskContentContainer self-stretch inline-flex justify-start items-center gap-[18px]">
             <div className="RiskScoreContainer w-[105px] h-14 px-2.5 py-[13px] bg-[#ffedef] rounded-[10px] outline outline-1 outline-offset-[-1px] outline-[#ffd4c7] flex justify-center items-center">
-              <div className="RiskScoreValue text-center text-[#8f0b09] text-2xl font-semibold">
+              <p className="RiskScoreValue text-center text-[#8f0b09] text-2xl font-semibold">
                 {riskScore}
-              </div>
+              </p>
             </div>
 
             <div className="RiskDescriptionContainer w-[228px] inline-flex flex-col justify-start items-start gap-0.5">
-              <div className="RiskDescriptionTitle self-stretch text-black text-xs font-semibold">
+              <p className="RiskDescriptionTitle self-stretch text-black text-xs font-semibold">
                 {riskTitle}
-              </div>
-              <div className="RiskDescriptionSubtext self-stretch text-black/60 text-xs">
+              </p>
+              <p className="RiskDescriptionSubtext self-stretch text-black/60 text-xs">
                 {riskSubtitle}
-              </div>
+              </p>
             </div>
           </div>
-
-        </div>
+        </section>
 
       </div>
-    </div>
+    </section>
   );
 }
 
